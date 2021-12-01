f8997f922c93b81644869c158ec4efd835d9fa4d
Scott Dickerson <swiftfoot@hotmail.com>
2021-11-30 19:03:10 -0600

    chore(video): test title overlaid video

--- src/App.js
+++ src/App.js
@@ -1,13 +1,17 @@
 import logo from './logo.svg';
 import './App.css';
+import eagleVideo from './video/01-WallofHonor-x1073304080-preview.mp4'
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
+        <video autoPlay muted loop>
+        <source src={eagleVideo} type="video/mp4"/>
+        </video> 
+        <p className="overlayTitle">
+          Overlaid title
         </p>
         <a
           className="App-link"
